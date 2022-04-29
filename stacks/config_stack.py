167a19b755c979b1002b7506a3e536cf42b3f235
Clark Schneider <cschneider@verticalrelevance.com>
2022-04-29 19:58:19 +0000

    add sqs_queue DemoChangeTrackedByConfig

--- stacks/config_stack.py
+++ stacks/config_stack.py
@@ -3,6 +3,7 @@ from aws_cdk import (
     Stack,
     aws_lambda,
     aws_config,
+    aws_sqs,
 )
 from constructs import Construct
 from utils import paths
@@ -12,7 +13,27 @@ class ControlBrokerConsumerExampleConfigStack(Stack):
 
     def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
-
+        
+        self.invoked_by_config()
+        self.demo_change_tracked_by_config()
+    
+    def demo_change_tracked_by_config(self):
+        
+        pass
+    
+        # toggle content-based deduplication to trigger change tracked by Config
+        
+        aws_sqs.Queue(
+            self,
+            "DemoChangeTrackedByConfig",
+            fifo = True,
+            content_based_deduplication = True,
+            # content_based_deduplication = False,
+        )
+    
+    
+    def invoked_by_config(self):
+        
         self.invoked_by_config = aws_lambda.Function(
             self,
             f"InvokedByConfig",
@@ -29,11 +50,9 @@ class ControlBrokerConsumerExampleConfigStack(Stack):
         self.custom_config_rule = aws_config.CustomRule(
             self,
             "SQS-PoC",
-            rule_scope=aws_config.RuleScope(
-                resource_types = [
-                    aws_config.ResourceType.SQS_QUEUE
-                ]
-            ),
+            rule_scope=aws_config.RuleScope.from_resources([
+                aws_config.ResourceType.SQS_QUEUE,
+            ]),
             lambda_function=self.invoked_by_config,
             configuration_changes=True,
         )
